2839db32d5e58fd7d7b5417f58b02fbbb7927868
JSneak <kim2087@students.d211.org>
2017-07-09 14:25:37 -0500

    Add input box validation

--- asset/js/app.js
+++ asset/js/app.js
@@ -57,17 +57,31 @@ function displayProjects(data){
 }
 
 function createProject(){
-  var newProjectRef = projectsRef.push();
-  newProjectRef.set({
-    author: document.getElementById("author").value,
-    name: document.getElementById("name").value,
-   	timestamp: getTimeStamp(),
-   	desc: document.getElementById("description").value,
-   	link: document.getElementById("liveLink").value,
-   	code: document.getElementById("codeLink").value,
-   	upvote: 0,
-   	featured: "false"
-  });
+	var inputs = [document.getElementById("author"),document.getElementById("name"),document.getElementById("description"),document.getElementById("liveLink"),document.getElementById("codeLink")]
+	var completed = true;
+	for(var i = 0;i<inputs.length;i++)
+	{
+		inputs[i].className = "input"
+		if(inputs[i].value == "" || undefined || null)
+		{
+			inputs[i].className += " is-danger"
+			completed = false;
+		}
+	}
+	if(completed == true)
+	{
+	  var newProjectRef = projectsRef.push();
+	  newProjectRef.set({
+	    author: inputs[0].value,
+	    name: inputs[1].value,
+	   	timestamp: getTimeStamp(),
+	   	desc: inputs[2].value,
+	   	link: inputs[3].value,
+	   	code: inputs[4].value,
+	   	upvote: 0,
+	   	featured: "false"
+	  });
+	}
 }
 
 function getTimeStamp(){
