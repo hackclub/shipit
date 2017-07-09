ef3362068f80b2e3efbfe0c829191a14e928d039
JSneak <kim2087@students.d211.org>
2017-07-09 10:10:44 -0500

    Move intialize firebase to app.js

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB3m0F90sr7i5r4ma8pAWimovacR8LCNLc",
+    authDomain: "shipit-f7171.firebaseapp.com",
+    databaseURL: "https://shipit-f7171.firebaseio.com",
+    projectId: "shipit-f7171",
+    storageBucket: "",
+    messagingSenderId: "454881075829"
+};
+firebase.initializeApp(config);
+
 //Database Control
 var database = firebase.database();
 
