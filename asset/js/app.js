b0b1b99548520759e97717512faf8601cb6efb46
Mingjie Jiang <jiang@mingjie.info>
2017-07-09 11:40:02 -0400

    Finish Shipment Templating

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,6 +1,33 @@
-//Database Control
-var database = firebase.database();
-
-database.ref('/').once('value', function(snapshot){
-  console.log(snapshot.val());
+$(function () {
+    loadShipment();
 });
+
+var shipment = $("#shipment-templ").html();
+var template = Handlebars.compile(shipment);
+
+//Sample - To be replaced by GET db content
+var context = {
+    0: {
+        author: "zachlatta",
+        timestamp: "11:09 PM - 7 Jul 2017",
+        desc: "We help high schoolers start awesome after-school coding clubs!",
+        link: "https://hackclub.com",
+        code: "https://github.com/hackclub/hackclub",
+        upvote: 255
+    },
+    1: {
+        author: "bgates",
+        timestamp: "12:00 AM - 25 Jun 1998 ",
+        desc: "The operating system for modern devices... Like my IBM ThinkPad i1300!",
+        link: "https://windows.microsoft.com",
+        code: "https://github.com/microsoft",
+        upvote: 255
+    }
+}
+
+function loadShipment() {
+    $("#shipped-placeholder").hide();
+    for (var i = 0; i < Object.keys(context).length; i++) {
+        $("#shipped").append(template(context[i]));
+    }
+}
