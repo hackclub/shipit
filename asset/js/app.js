177d5e6d91b1c6168d04deab85d88e373d4a880f
JSneak <kim2087@students.d211.org>
2017-07-09 10:14:45 -0500

    Change credentials

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyB3m0F90sr7i5r4ma8pAWimovacR8LCNLc",
-    authDomain: "shipit-f7171.firebaseapp.com",
-    databaseURL: "https://shipit-f7171.firebaseio.com",
-    projectId: "shipit-f7171",
-    storageBucket: "",
-    messagingSenderId: "454881075829"
+	apiKey: "AIzaSyD-IT1RWXi-7bMSjtTsPmpaTD2SXadFxC0",
+	authDomain: "shipit-7427d.firebaseapp.com",
+	databaseURL: "https://shipit-7427d.firebaseio.com",
+	projectId: "shipit-7427d",
+	storageBucket: "",
+	messagingSenderId: "601650858338"
 };
 firebase.initializeApp(config);
 
