4f746f81db57ede9bfdf97e3df39270349dae10f
Mingjie Jiang <jiang@mingjie.info>
2017-07-09 16:44:14 -0400

    Generate HackID and refresh UI

--- asset/js/app.js
+++ asset/js/app.js
@@ -80,3 +80,15 @@ function getTimeStamp(){
 	console.log(date)
 	return date;
 }
+
+function generateHackID() {
+  var d = new Date().getTime();
+
+  var uuid = 'xxxxx'.replace(/[xy]/g, function (c) {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return ((c == 'x' ? r : (r & 0x3 | 0x8)).toString(16)).toUpperCase();
+  });
+
+  return uuid;
+}
