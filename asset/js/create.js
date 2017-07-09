037dd71b48cc44af6321bea5a954c01654f21a7c
JSneak <kim2087@students.d211.org>
2017-07-09 11:17:33 -0500

    Create getTimeStamp()

--- asset/js/create.js
+++ asset/js/create.js
@@ -9,17 +9,34 @@ var config = {
 };
 firebase.initializeApp(config);
 
+const database = firebase.database();
 const projectsRef = database.ref("/projects");
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 function createProject(){
   var newProjectRef = projectsRef.push();
   newProjectRef.set({
     author: document.getElementById("name").value,
-   	timestamp: document.getElementById("time").value,
+   	timestamp: getTimeStamp(),
    	desc: document.getElementById("description").value,
    	link: document.getElementById("liveLink").value,
    	code: document.getElementById("codeLink").value,
    	upvote: 0,
    	featured: "false"
   });
+}
+
+function getTimeStamp(){
+	var currentDate = new Date();
+  var hours = currentDate.getHours();
+  var minutes = currentDate.getMinutes();
+  var dd = currentDate.getDate();
+	var mm = monthNames[currentDate.getMonth()]
+	var yyyy = currentDate.getFullYear();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime + " - " +dd + " " + mm + " " +yyyy;
 }
