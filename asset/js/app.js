075245faf9fd90666edb44d00da7616539076211
JSneak <kim2087@students.d211.org>
2017-07-09 21:45:20 -0500

    Delete Duplicate Github

--- asset/js/app.js
+++ asset/js/app.js
@@ -165,33 +165,3 @@ function getProp(id){
 function buildPage(data,key){
 	displayProjects(data,key)
 }
-
-var provider = new firebase.auth.GithubAuthProvider();
-
-function githubSignin() {
-   firebase.auth().signInWithPopup(provider)
-   
-   .then(function(result) {
-      var token = result.credential.accessToken;
-      var user = result.user;
-		
-      console.log(token)
-      console.log(user)
-   }).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-		
-      console.log(error.code)
-      console.log(error.message)
-   });
-}
-
-function githubSignout(){
-   firebase.auth().signOut()
-   
-   .then(function() {
-      console.log('Signout successful!')
-   }, function(error) {
-      console.log('Signout failed')
-   });
-}
