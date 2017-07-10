1e254c9e0494b1d06e9d79ae9d085f2d97916c26
Mingjie Jiang <jiang@mingjie.info>
2017-07-09 20:34:26 -0400

    Add getParams

--- asset/js/render.js
+++ asset/js/render.js
@@ -1,3 +1,10 @@
+$(function() {
+    var shared = getParams("shared");
+    if (shared != null) {
+        //Stop running get all, only display the requested card
+    }
+});
+
 var shipment = $("#shipment-templ").html();
 var template = Handlebars.compile(shipment);
 
@@ -31,4 +38,14 @@ function closeShipper() {
 
 function shareShipment(uuid) {
     $("#share-modal").addClass("is-active");
+}
+
+function getParams(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
