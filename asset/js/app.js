ead7321a6518d9da83cc31cd036a8b7b84b2f54a
JSneak <kim2087@students.d211.org>
2017-07-16 18:33:04 -0500

    Simpler url check

--- asset/js/app.js
+++ asset/js/app.js
@@ -160,11 +160,11 @@ function createProject() {
         if (inputs[5].value) {
             completed = false;
         }
-        if (!checkIfValidURL(inputs[3].value, false)) {
+        if (!checkIfValidURL(inputs[3].value)) {
             completed = false;
             inputs[3].className += " is-danger";
         }
-        if (!checkIfValidURL(inputs[4].value, true)) {
+        if (!checkIfValidURL(inputs[4].value)) {
             completed = false;
             inputs[4].className += " is-danger";
         }
@@ -197,11 +197,7 @@ function createProject() {
 }
 
 function checkIfValidURL(value, ssl) {
-    if (ssl) {
-        return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
-    } else {
-        return /^(?:(?:(?:http?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
-    }
+    return value.substring(0,4) == "http"
 }
 
 function convertTimestamp(id) {
