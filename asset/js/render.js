669948f3657250c77898e9ed4d8c9290388990ba
Mingjie Jiang <jiang@mingjie.info>
2017-07-28 18:43:17 -0400

    Fix Google Analytics

--- asset/js/render.js
+++ asset/js/render.js
@@ -10,17 +10,6 @@ toastr.options.closeEasing = 'linear';
 
 toastr.options.closeButton = true;
 
-//Google Analytics Code
-(function (i, s, o, g, r, a, m) {
-i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-    (i[r].q = i[r].q || []).push(arguments)
-}, i[r].l = 1 * new Date(); a = s.createElement(o),
-    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-ga('create', 'UA-103577028-1', 'auto');
-ga('send', 'pageview');
-
 function loadShipment(id) {
     $("#shipped-placeholder").slideUp("slow");
     $("#shipped").prepend(template(id));
