da9f8f531c3879da19c55e5a3c017cf653923b73
Mingjie Jiang <jimmy.m.jiang@aol.com>
2018-01-05 03:32:20 +0000

    Production base switching

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,13 +1,13 @@
-// Initialize Firebase - DEV BASE
-var config = {
-    apiKey: "AIzaSyDxvQeq7iqqfh2ZZeJZMWyLxXCX0KPMoBs",
-    authDomain: "shipit-alpha.firebaseapp.com",
-    databaseURL: "https://shipit-alpha.firebaseio.com",
-    projectId: "shipit-alpha",
-    storageBucket: "shipit-alpha.appspot.com",
-    messagingSenderId: "740447864312"
-};
-firebase.initializeApp(config);
+// Initialize Firebase - PRODUCTION BASE
+ var config = {
+    apiKey: "AIzaSyD-IT1RWXi-7bMSjtTsPmpaTD2SXadFxC0",
+    authDomain: "shipit-7427d.firebaseapp.com",
+    databaseURL: "https://shipit-7427d.firebaseio.com",
+    projectId: "shipit-7427d",
+    storageBucket: "shipit-7427d.appspot.com",
+    messagingSenderId: "601650858338"
+  };
+  firebase.initializeApp(config);
 
 //Database Control
 const provider = new firebase.auth.GithubAuthProvider();
