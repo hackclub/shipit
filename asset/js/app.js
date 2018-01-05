c0e1a2e06114be36989ffb4098780d89c6083db1
Mingjie Jiang <jiang@mingjie.info>
2018-01-04 21:18:55 -0500

    Switch back to production base

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,11 +1,11 @@
-// Initialize Firebase - DEV BASE
+// Initialize Firebase - PRODUCTION BASE
 var config = {
-    apiKey: "AIzaSyDxvQeq7iqqfh2ZZeJZMWyLxXCX0KPMoBs",
-    authDomain: "shipit-alpha.firebaseapp.com",
-    databaseURL: "https://shipit-alpha.firebaseio.com",
-    projectId: "shipit-alpha",
-    storageBucket: "shipit-alpha.appspot.com",
-    messagingSenderId: "740447864312"
+    apiKey: "AIzaSyD-IT1RWXi-7bMSjtTsPmpaTD2SXadFxC0",
+    authDomain: "shipit-7427d.firebaseapp.com",
+    databaseURL: "https://shipit-7427d.firebaseio.com",
+    projectId: "shipit-7427d",
+    storageBucket: "shipit-7427d.appspot.com",
+    messagingSenderId: "601650858338"
 };
 firebase.initializeApp(config);
 
