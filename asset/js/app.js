060454fdc6d4eb53c9079c4ea38f0c0d1e6fbfd7
Mingjie Jiang <jiang@mingjie.info>
2018-01-05 21:56:56 -0500

    Seperate config file

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,14 +1,3 @@
-// Initialize Firebase - PRODUCTION BASE
-var config = {
-    apiKey: "AIzaSyD-IT1RWXi-7bMSjtTsPmpaTD2SXadFxC0",
-    authDomain: "shipit-7427d.firebaseapp.com",
-    databaseURL: "https://shipit-7427d.firebaseio.com",
-    projectId: "shipit-7427d",
-    storageBucket: "shipit-7427d.appspot.com",
-    messagingSenderId: "601650858338"
-  };
-  firebase.initializeApp(config);
-
 //Database Control
 const provider = new firebase.auth.GithubAuthProvider();
 const database = firebase.database();
---
+++ asset/js/config.dev.js
@@ -0,0 +1,10 @@
+// Initialize Firebase - DEV BASE
+var config = {
+    apiKey: "AIzaSyDxvQeq7iqqfh2ZZeJZMWyLxXCX0KPMoBs",
+    authDomain: "shipit-alpha.firebaseapp.com",
+    databaseURL: "https://shipit-alpha.firebaseio.com",
+    projectId: "shipit-alpha",
+    storageBucket: "shipit-alpha.appspot.com",
+    messagingSenderId: "740447864312"
+};
+firebase.initializeApp(config);
